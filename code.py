016cc406168c0a8967317de7f864a44b2d998b8b
Obierefu Finbarr Chukwuka <57522378+finbarr91@users.noreply.github.com>
2021-02-11 13:22:30 -0800

    Create code.py

---
+++ code.py
@@ -0,0 +1,2 @@
+# Naval-mine-classifier-Support-Vector-machine
+This work involves the classification of sonar signals using a Support Vector machine written in Python .  The task is to train a model to discriminate between sonar signals bounced off a metal cylinder and those bounced off a roughly cylindrical rock.
